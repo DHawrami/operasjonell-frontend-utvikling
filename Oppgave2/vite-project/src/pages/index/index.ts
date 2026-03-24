cb084c5aaae2c3830194c86590099d40ac521d38
Dastan Salah Hawrami <dastan.hawrami@tetdigital.no>
2026-03-24 18:56:16 +0100

    test

--- Oppgave2/vite-project/src/pages/index/index.ts
+++ Oppgave2/vite-project/src/pages/index/index.ts
@@ -1,9 +1,9 @@
  console.log('Hello from index.ts!');
 async function fetchData() {
   try {
-    const response = await fetch('http://localhost:3000/api/rooms');
+    const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
-    console.log(data[0]);
+    console.log(data);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
