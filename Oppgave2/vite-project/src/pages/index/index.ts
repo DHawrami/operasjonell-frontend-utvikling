ece23050dae336795a2e5e247427a4b3630f8944
DHawrami <dastan.hawrami@ruter.no>
2026-03-25 14:17:11 +0100

    index.ts

--- Oppgave2/vite-project/src/pages/index/index.ts
+++ Oppgave2/vite-project/src/pages/index/index.ts
@@ -1,34 +1,33 @@
- console.log('Hello from index.ts!');
+console.log("Hello from index.ts!");
 async function fetchData() {
   try {
-    const response = await fetch('http://localhost:3000/api/users');
+    const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
-    console.log(data);
+    console.log(data[0]);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 }
 fetchData();
 
 async function fetchData() {
   try {
-    const response = await fetch('http://localhost:3000/api/rooms');
+    const response = await fetch("http://localhost:3000/api/rooms");
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 }
 fetchData();
 
-
 async function fetchData() {
   try {
-    const response = await fetch('http://localhost:3000/api/bookings');
+    const response = await fetch("http://localhost:3000/api/bookings");
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 }
 fetchData();
